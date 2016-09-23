e1301f5705a2a498b19116fcb486476117e0ebd5
EK3310 <EK3310@SK11J600-86>
2016-09-23 16:37:12 +0800

    加入專案檔案。

---
+++ MyDialog/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyDialog.Models;
+using MyDialog.Data;
+
+namespace MyDialog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<CarModel> model = CarRepository.GetCars();
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            CarModel model = CarRepository.GetCars().Where(c => c.Id == id).FirstOrDefault();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CarModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                CarModel car = CarRepository.GetCars().Where(c => c.Id == model.Id).FirstOrDefault();
+                car.Name = model.Name;
+                car.Description = model.Description;
+                return Content(Boolean.TrueString);
+            }
+            else
+            {
+                return Content("Please review your form");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MyDialog/Data/CarRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyDialog.Models;
+namespace MyDialog.Data
+{
+    public static class CarRepository
+    {
+        static List<CarModel> CarList;
+
+        static CarRepository()
+        {
+            CarList = new List<CarModel>{
+                new CarModel { Id=1, Name="Ford Focus Electric", Description = "Great looking car... plus is green."},
+                new CarModel { Id=2, Name="Honda Civic 2012", Description="You can always count with the Civic"},
+                new CarModel { Id=3, Name="Hyundai HCD 12", Description="Can't wait for it to come out!"},
+                new CarModel { Id=4, Name="Toyota Prius C", Description="What does the C stands for?"}};
+        }
+
+        /// <summary>
+        /// NOTE: This is for demostration purposes only. 
+        ///         You would normally have your data access layer, either on the web app or in other libraries.
+        /// </summary>
+        /// <returns></returns>
+        public static List<CarModel> GetCars()
+        {
+            return CarList;
+        }
+    }
+}
